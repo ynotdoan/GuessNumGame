2984bbfec78fe757581c1a304ec5d184c0aa9964
Tony Doan <ynotdoan@gmail.com>
2020-11-09 15:42:47 -0800

    added print statements for beginning

--- GuessNum.py
+++ GuessNum.py
@@ -1,1 +1,6 @@
 
+print ("This program will generate a random number in a given number range. ")
+print ("Then the user, you, will be given unlimited tries to guess the right number. ")
+print ()
+min = input ("Please enter the minimum number for the range: ")
+max = input ("Please enter the maximum number for the range: ")
